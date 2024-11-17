b068ecfc696f3e2dc09036310466511c5df6b278
AtheeqAhammed <atheeqahammed@gmail.com>
2024-11-17 16:14:20 +0530

    Initial Commit

---
+++ EcomSwiftUI/EcomSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  EcomSwiftUIApp.swift
+//  EcomSwiftUI
+//
+//  Created by Ateeq Ahmed on 17/11/24.
+//
+
+import SwiftUI
+
+@main
+struct EcomSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
